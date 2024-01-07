7b74406a73cdcd01740448dfe5686b4729361e75
NaorBD <naorbend@gmail.com>
2024-01-07 11:38:39 +0200

    calculator ui first prototype

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+error_list = []
+
+def calculate():
+    while True:
+        calc_str = interface()
+        check_&_ret_value(calc_str)
+        if not error_list:
+            badui()
+        escape: bool = end(escape)
+        if escape:
+            return
+
+
+
 def main():
-    return 0
+    calculate()
+    return
 
 
 if __name__ == "__main__":
